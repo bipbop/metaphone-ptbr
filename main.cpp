fcdf3ec3e06e13fa02844d0d032a7e83363236bc
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2018-12-27 18:52:21 -0200

    metaphone ptbr

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include <nan.h>
 #include <iostream>
+#include <nan.h>
 
 extern "C" {
-  #include <metaphone_ptbr.h>
+#include <metaphone_ptbr.h>
 }
 
-using namespace Nan;  // NOLINT(build/namespaces)
+using namespace Nan; // NOLINT(build/namespaces)
 
 NAN_METHOD(metaphone) {
   std::string stringArgument = *v8::String::Utf8Value(info[0]->ToString());
@@ -16,17 +16,17 @@ NAN_METHOD(metaphone) {
     return;
   }
 
-  wchar_t * stringArgumentEncoded = (wchar_t *)malloc(stringArgumentLength * sizeof(wchar_t));
-  std::mbstowcs(stringArgumentEncoded, stringArgument.c_str(), stringArgumentLength);
-  char * metaphone = Metaphone_PTBR(stringArgumentEncoded, stringArgumentLength);
+  wchar_t *stringArgumentEncoded =
+      (wchar_t *)malloc(stringArgumentLength * sizeof(wchar_t));
+  std::mbstowcs(stringArgumentEncoded, stringArgument.c_str(),
+                stringArgumentLength);
+  char *metaphone = Metaphone_PTBR(stringArgumentEncoded, stringArgumentLength);
   free(stringArgumentEncoded);
 
   info.GetReturnValue().Set(New(metaphone).ToLocalChecked());
   free(metaphone);
 }
 
-NAN_MODULE_INIT(Initialize) {
-    NAN_EXPORT(target, metaphone);
-}
+NAN_MODULE_INIT(Initialize) { NAN_EXPORT(target, metaphone); }
 
 NODE_MODULE(addon, Initialize);
